09e688ff5aaf24e32e09631c0cbb1cc84b6604ce
henriqueweege <henriqueweege@gmail.com>
2023-10-09 15:24:20 -0300

    implementando multiplication

--- Calculadora/Calculadora.UnitTests/Operations_UnitTests.cs
+++ Calculadora/Calculadora.UnitTests/Operations_UnitTests.cs
@@ -205,5 +205,108 @@ namespace Calculadora.UnitTests
 
 
         #endregion
+
+
+        #region Multiplication
+
+        [Theory]
+        [InlineData(1, 2)]
+        [InlineData(10, 20)]
+        [InlineData(19, 4)]
+        [InlineData(1359, 490)]
+        [InlineData(-1, 2)]
+        [InlineData(-10, 20)]
+        [InlineData(-19, 4)]
+        [InlineData(-1359, 490)]
+        [InlineData(1, -2)]
+        [InlineData(10, -20)]
+        [InlineData(19, -4)]
+        [InlineData(1359, -490)]
+        public void GivenIntegerNumbers_Multiplication_ShouldReturnCorrectValue(int n1, int n2)
+        {
+            //arrange
+            //act
+            double result = _operations.Multiplication(n1, n2);
+
+            //assert
+            Assert.Equal(n1 * n2, result);
+        }
+
+        [Theory]
+        [InlineData(11.586, 3.452)]
+        [InlineData(106.9, 21.20)]
+        [InlineData(1.69, 4.3)]
+        [InlineData(135.9, 4.90)]
+        [InlineData(-11.586, 3.452)]
+        [InlineData(-106.9, 21.20)]
+        [InlineData(-1.69, 4.3)]
+        [InlineData(-135.9, 4.90)]
+        [InlineData(11.586, -3.452)]
+        [InlineData(106.9, -21.20)]
+        [InlineData(1.69, -4.3)]
+        [InlineData(135.9, -4.90)]
+        public void GivenDoubleNumbers_Multiplication_ShouldReturnCorrectValue(double n1, double n2)
+        {
+            //arrange
+            //act
+            double result = _operations.Multiplication(n1, n2);
+
+            //assert
+            Assert.Equal(n1 * n2, result);
+
+        }
+
+        [Theory]
+        [InlineData(11.586, 3452)]
+        [InlineData(106.9, 21)]
+        [InlineData(1.69, 46784)]
+        [InlineData(135.9, 4)]
+        [InlineData(-11.586, 3452)]
+        [InlineData(-106.9, 21)]
+        [InlineData(-1.69, 46784)]
+        [InlineData(-135.9, 4)]
+        [InlineData(11.586, -3452)]
+        [InlineData(106.9, -21)]
+        [InlineData(1.69, -46784)]
+        [InlineData(135.9, -4)]
+        public void GivenN1AsDoubleAndN2AsInteger_Multiplication_ShouldReturnCorrectValue(double n1, int n2)
+        {
+            //arrange
+
+            //act
+            double result = _operations.Multiplication(n1, n2);
+
+            //assert
+            Assert.Equal(n1 * n2, result);
+
+        }
+
+        [Theory]
+        [InlineData(3452, 11.586)]
+        [InlineData(21, 106.9)]
+        [InlineData(46784, 1.69)]
+        [InlineData(4, 135.9)]
+        [InlineData(-3452, 11.586)]
+        [InlineData(-21, 106.9)]
+        [InlineData(-46784, 1.69)]
+        [InlineData(-4, 135.9)]
+        [InlineData(3452, -11.586)]
+        [InlineData(21, -106.9)]
+        [InlineData(46784, -1.69)]
+        [InlineData(4, -135.9)]
+        public void GivenN2AsDoubleAndN1AsInteger_Multiplication_ShouldReturnCorrectValue(int n1, double n2)
+        {
+            //arrange
+
+            //act
+            double result = _operations.Multiplication(n1, n2);
+
+            //assert
+            Assert.Equal(n1 * n2, result);
+
+        }
+
+
+        #endregion
     }
 }
--- Calculadora/Calculadora/Domain/Operations.cs
+++ Calculadora/Calculadora/Domain/Operations.cs
@@ -4,5 +4,7 @@
     {
         public double Sum(double n1, double n2) => n1 + n2;
         public double Subtraction(double n1, double n2) => n1 - n2;
+
+        public double Multiplication(double n1, double n2) => n1 * n2;
     }
 }
