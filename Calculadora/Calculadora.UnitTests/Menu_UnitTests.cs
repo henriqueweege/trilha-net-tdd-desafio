cc658dcc019e1f2f37baba7b67247204e647a9ce
henriqueweege <henriqueweege@gmail.com>
2023-10-09 18:50:28 -0300

    criando menu handler

---
+++ Calculadora/Calculadora.UnitTests/Menu_UnitTests.cs
@@ -0,0 +1,13 @@
+﻿using Calculadora.Handlers;
+
+namespace Calculadora.UnitTests
+{
+    public class Menu_UnitTests
+    {
+        private MenuHandler _menuHandler;
+        public Menu_UnitTests()
+        {
+            _menuHandler = new MenuHandler();
+        }
+    }
+}
