e2dd360a9c94ed468bef8ccd637c6cf7ec29a4ca
henriqueweege <henriqueweege@gmail.com>
2023-10-09 15:42:08 -0300

    renomeando cache para cachehandler

--- Calculadora/Calculadora.UnitTests/Cache_UnitTests.cs
+++ Calculadora/Calculadora.UnitTests/Cache_UnitTests.cs
@@ -3,18 +3,20 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Calculadora.Handlers;
 
 namespace Calculadora.UnitTests
 {
     public class Cache_UnitTests
     {
-        private readonly Cache _cache;
+        private readonly CacheHandler _cache;
         public Cache_UnitTests()
         {
-            _cache = new Cache();
+            _cache = new CacheHandler();
         }
 
-
+        //[Fact]
+        //public void GivenOperationToSave_
 
 
     }
--- Calculadora/Calculadora/Handlers/CacheHandler.cs
+++ Calculadora/Calculadora/Handlers/CacheHandler.cs
@@ -4,9 +4,9 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace Calculadora
+namespace Calculadora.Handlers
 {
-    public class Cache
+    public class CacheHandler
     {
     }
 }
