b000c2989b58a655feb1e371a71b2006ac2d383a
henriqueweege <henriqueweege@gmail.com>
2023-10-09 14:18:06 -0300

    criando classe Operations

---
+++ Calculadora/Calculadora.UnitTests/Operations_UnitTests.cs
@@ -0,0 +1,20 @@
+﻿using Calculadora.Domain;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Calculadora.UnitTests
+{
+    public class Operations_UnitTests
+    {
+        private readonly Operations _operations;
+        public Operations_UnitTests()
+        {
+            _operations = new Operations(); 
+        }
+
+        
+    }
+}
