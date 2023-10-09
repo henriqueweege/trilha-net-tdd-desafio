b4f75b9ff997123341df715e8314e61de4aeda35
henriqueweege <henriqueweege@gmail.com>
2023-10-09 14:29:06 -0300

    criando mais testes

--- Calculadora/Calculadora.UnitTests/Operations_UnitTests.cs
+++ Calculadora/Calculadora.UnitTests/Operations_UnitTests.cs
@@ -15,13 +15,14 @@ namespace Calculadora.UnitTests
             _operations = new Operations(); 
         }
 
-        [Fact]
-        public void GivenIntegerNumbers_Sum_ShouldReturnCorrectValue()
+        [Theory]
+        [InlineData(1, 2)]
+        [InlineData(10, 20)]
+        [InlineData(19, 4)]
+        [InlineData(1359, 490)]
+        public void GivenIntegerNumbers_Sum_ShouldReturnCorrectValue(int n1, int n2)
         {
             //arrange
-            var n1 = 1;
-            var n2 = 2;
-
             //act
             int result = _operations.Sum(n1, n2);
 
@@ -29,12 +30,45 @@ namespace Calculadora.UnitTests
             Assert.Equal(n1 + n2, result);
         }
 
-        [Fact]
-        public void GivenDoubleNumbers_Sum_ShouldReturnCorrectValue()
+        [Theory]
+        [InlineData(11.586, 3.452)]
+        [InlineData(106.9, 21.20)]
+        [InlineData(1.69, 4.3)]
+        [InlineData(135.9, 4.90)]
+        public void GivenDoubleNumbers_Sum_ShouldReturnCorrectValue(double n1,double n2)
         {
             //arrange
-            var n1 = 1.1;
-            var n2 = 2.2;
+            //act
+            double result = _operations.Sum(n1, n2);
+
+            //assert
+            Assert.Equal(n1 + n2, result);
+        }
+
+        [Theory]
+        [InlineData(11.586, 3452)]
+        [InlineData(106.9, 21)]
+        [InlineData(1.69, 46784)]
+        [InlineData(135.9, 4)]
+        public void GivenN1AsDoubleAndN2AsInteger_Sum_ShouldReturnCorrectValue(double n1, int n2)
+        {
+            //arrange
+
+            //act
+            double result = _operations.Sum(n1, n2);
+
+            //assert
+            Assert.Equal(n1 + n2, result);
+        }
+
+        [Theory]
+        [InlineData(3452, 11.586)]
+        [InlineData(21, 106.9)]
+        [InlineData(46784, 1.69)]
+        [InlineData(4, 135.9)]
+        public void GivenN2AsDoubleAndN1AsInteger_Sum_ShouldReturnCorrectValue(int n1, double n2)
+        {
+            //arrange
 
             //act
             double result = _operations.Sum(n1, n2);
