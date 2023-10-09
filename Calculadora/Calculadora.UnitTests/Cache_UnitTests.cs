03d5afdc6d5827450c97edb3a81a7d861deb9d31
henriqueweege <henriqueweege@gmail.com>
2023-10-09 15:39:55 -0300

    criando classe chache

---
+++ Calculadora/Calculadora.UnitTests/Cache_UnitTests.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Calculadora.UnitTests
+{
+    public class Cache_UnitTests
+    {
+        private readonly Cache _cache;
+        public Cache_UnitTests()
+        {
+            _cache = new Cache();
+        }
+
+
+
+
+    }
+}
