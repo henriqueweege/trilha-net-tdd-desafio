35d1fb56624b4843632043f90ec4c122ced6460f
henriqueweege <henriqueweege@gmail.com>
2023-10-09 19:24:39 -0300

    implementando getlastoperations

--- Calculadora/Calculadora.UnitTests/OutputHandler_UnitTests.cs
+++ Calculadora/Calculadora.UnitTests/OutputHandler_UnitTests.cs
@@ -38,17 +38,27 @@ namespace Calculadora.UnitTests
             var operations = new List<Operation>();
             operations.Add(new(1, 2, Domain.Enum.EOperationType.Sum));
             operations.Add(new(2, 3, Domain.Enum.EOperationType.Subtraction));
-            operations.Add(new(3, 3, Domain.Enum.EOperationType.Multiplication));
+            operations.Add(new(3, 4, Domain.Enum.EOperationType.Multiplication));
 
             //act
-            string menu = _outputHandler.GetOperationsMenu();
+            string lastOperations = _outputHandler.GetLastOperations(operations);
 
             //assert
-            //Assert.Contains(sum, menu);
-            //Assert.Contains(subtraction, menu);
-            //Assert.Contains(division, menu);
-            //Assert.Contains(multiplication, menu);
+            Assert.Contains("Operação 1", lastOperations);
+            Assert.Contains("N1: 1", lastOperations);
+            Assert.Contains("N2: 2", lastOperations);
+            Assert.Contains("Resultado: 3", lastOperations);
+            Assert.Contains("Operação 2", lastOperations);
+            Assert.Contains("N1: 2", lastOperations );
+            Assert.Contains("N2: 3", lastOperations );
+            Assert.Contains("Resultado: -1", lastOperations     );
+            Assert.Contains("Operação 3", lastOperations);
+            Assert.Contains("N1: 3", lastOperations);
+            Assert.Contains("N2: 4", lastOperations);
+            Assert.Contains("Resultado: 12", lastOperations);
         }
+
+
 
 
     }
--- Calculadora/Calculadora/Handlers/OutputHandler.cs
+++ Calculadora/Calculadora/Handlers/OutputHandler.cs
@@ -1,10 +1,23 @@
-﻿namespace Calculadora.Handlers
+﻿using Calculadora.Domain;
+using System.Text;
+
+namespace Calculadora.Handlers
 {
     public class OutputHandler
     {
-        public string GetOperationsMenu()
+        public string GetLastOperations(List<Operation> operations)
         {
-            return $"Qual operação você gostaria de realizar? \n\n  1-Soma \n 2-Subtração \n 3-Multiplicação, \n 4-Divisão";
+            var toReturn = new StringBuilder();
+            for(var i =0; i < operations.Count; i++)
+            {
+                toReturn.AppendLine($"\n Operação {i + 1} \n N1: {operations[i].N1} \n N2: {operations[i].N2} \n Resultado: {operations[i].Result} \n\n");
+            }
+
+            return toReturn.ToString();
         }
+
+        public string GetOperationsMenu()
+            => $"Qual operação você gostaria de realizar? \n\n  1-Soma \n 2-Subtração \n 3-Multiplicação, \n 4-Divisão";
+        
     }
 }
